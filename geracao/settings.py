27970871c1e9dfe49c51bb90098a02ebf5e30dcd
GabrielHenri39 <gabriel688dias@hotmail.com>
2022-05-14 12:11:14 -0300

    python-decouple

--- geracao/settings.py
+++ geracao/settings.py
@@ -153,13 +153,13 @@ AUTH_USER_MODEL = 'autenticacao.User'
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-    EMAIL_HOST = config('EMAIL_HOST', cast=str, default='smtp.gmail.com')
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD',default='.ENV')
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='.ENV')
+    EMAIL_HOST = config('EMAIL_HOST',  default='smtp.gmail.com')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD',default='')
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
     EMAIL_PORT = config('EMAIL_PORT',cast=int,default=587)
     EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool,default=True)
 
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='.ENV')
+    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
 
 django_on_heroku.settings(locals())
